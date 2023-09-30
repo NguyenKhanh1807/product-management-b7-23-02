2173f37b5ca6d143ae3c707d39c0a6e84bf601de
NguyenKhanh1807 <138544322+NguyenKhanh1807@users.noreply.github.com>
2023-09-30 22:51:06 +0700

    1. Validate dữ liệu

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -10,6 +10,8 @@ const upload = multer({ storage: storage });
 
 const controller = require("../../controllers/admin/product.controller");
 
+const validate = require("../../validates/admin/product.validate");
+
 router.get("/", controller.index);
 
 router.patch("/change-status/:status/:id", controller.changeStatus);
@@ -20,6 +22,12 @@ router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create", controller.create);
 
-router.post("/create", upload.single("thumbnail"), controller.createPost);
+router.post
+(
+    "/create", 
+    upload.single("thumbnail"),
+    validate.createPost, 
+    controller.createPost
+);
 
 module.exports = router;

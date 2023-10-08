b43c0fce8847a28c8f9ac22947da6841272fd1b3
NguyenKhanh1807 <138544322+NguyenKhanh1807@users.noreply.github.com>
2023-10-08 12:17:00 +0700

    1. Tính năng chỉnh sửa danh mục sản phẩm

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,6 +2,7 @@ const systemConfig = require("../../config/system");
 
 const dashboardRoutes = require("./dashboard.route");
 const productRoutes = require("./product.route");
+const productCategoryRoutes = require("./product-category.route");
 
 module.exports = (app) => {
 
@@ -10,4 +11,6 @@ module.exports = (app) => {
     app.use("/admin/dashboard", dashboardRoutes);
 
     app.use(PATH_ADMIN + "/products", productRoutes);
+
+    app.use(PATH_ADMIN + "/products-category", productCategoryRoutes);
 }

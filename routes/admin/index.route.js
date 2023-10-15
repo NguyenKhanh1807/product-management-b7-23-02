2db2abb7de27a6f965694ae475f92df15e254a72
NguyenKhanh1807 <138544322+NguyenKhanh1807@users.noreply.github.com>
2023-10-15 13:13:52 +0700

    chỉnh sửa trang thông tin cá nhân + menu cho trang client

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -14,6 +14,8 @@ const authRoutes = require("./auth.route");
 
 const authMiddleware = require("../../middlewares/admin/auth.middleware");
 
+const myAccountRoutes = require("./my-account.route");
+
 module.exports = (app) => {
 
     const PATH_ADMIN = "/" + systemConfig.prefixAdmin;
@@ -33,4 +35,6 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/accounts",authMiddleware.requireAuth, accountRoutes);
 
     app.use(PATH_ADMIN + "/auth", authRoutes);
+
+    app.use(PATH_ADMIN + "/my-account", authMiddleware.requireAuth, myAccountRoutes);
 }
